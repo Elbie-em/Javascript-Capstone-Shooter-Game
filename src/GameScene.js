8d75c4b001dfd452251e49421d288e5c249a5cc3
Elbie-em <emoonga1993@gmail.com>
2020-09-08 11:45:11 +0200

    Load player sprite to Game scene

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,13 +1,21 @@
 class GameScene extends Phaser.Scene {
     constructor() {
-        super({key: "GameScene"});
+        super({ key: "GameScene" });
     }
 
     preload() {
-        
+        this.load.spritesheet("spaceShipPlayer", "assets/playerSprite.png", {
+            frameWidth: 132,
+            frameHeight: 20
+        });
     }
 
     create() {
-
+        this.anims.create({
+            key: "spaceShipPlayer",
+            frames: this.anims.generateFrameNumbers("spaceShipPlayer"),
+            frameRate: 20,
+            repeat: -1
+        });
     }
 }
