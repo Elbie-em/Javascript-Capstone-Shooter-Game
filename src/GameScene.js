e254ac9a80e3792e7f385e36319ea01747b419fd
Elbie-em <emoonga1993@gmail.com>
2020-09-08 12:46:11 +0200

    Add player sprite; controls and animation

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,21 +1,48 @@
 class GameScene extends Phaser.Scene {
-    constructor() {
-        super({ key: "GameScene" });
-    }
+	constructor() {
+		super({ key: "GameScene" });
+	}
 
-    preload() {
-        this.load.spritesheet("spaceShipPlayer", "assets/playerSprite.png", {
-            frameWidth: 132,
-            frameHeight: 20
-        });
-    }
+	preload() {
+		this.load.spritesheet("spaceShipPlayer", "../assets/playerSprite.png", {
+			frameWidth: 132,
+			frameHeight: 22
+		});
+	}
 
-    create() {
-        this.anims.create({
-            key: "spaceShipPlayer",
-            frames: this.anims.generateFrameNumbers("spaceShipPlayer"),
-            frameRate: 20,
-            repeat: -1
-        });
-    }
+	create() {
+		this.anims.create({
+			key: "spaceShipPlayer",
+			frames: this.anims.generateFrameNumbers("spaceShipPlayer"),
+			frameRate: 20,
+			repeat: -1
+		});
+
+		this.player = new Player(
+			this,
+			this.game.config.width * 0.5,
+			this.game.config.height * 0.5,
+			"spaceShipPlayer"
+		); 
+
+	}
+
+	update() {
+		this.player.update();
+		const cursors = this.input.keyboard.createCursorKeys();
+
+		if (cursors.up.isDown) {
+			this.player.moveUp();
+		}
+		else if (cursors.down.isDown) {
+			this.player.moveDown();
+		}
+		
+		if (cursors.left.isDown) {
+			this.player.moveBack();
+		}
+		else if (cursors.right.isDown) {
+			this.player.moveFront();
+		}
+	}
 }
