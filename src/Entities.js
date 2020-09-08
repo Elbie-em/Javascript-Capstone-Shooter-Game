f14cf065aced53997dbda6f2681cef809a073308
Elbie-em <emoonga1993@gmail.com>
2020-09-08 17:08:34 +0200

    Add baby ship class, invoke autoshoot timer in Mother ship

--- src/Entities.js
+++ src/Entities.js
@@ -52,5 +52,34 @@ class MotherShip extends Entity {
 		super(scene, x, y, "motherShip", "MotherShip");
 		this.play("motherShip");
 		this.body.setVelocity(-20,0)
+		this.shootTimer = this.scene.time.addEvent({
+			delay: 1000,
+			callback: () => {
+				var babyShip = new BabyShip(
+					this.scene,
+					this.x,
+					this.y + 20
+				);
+				babyShip.setScale(this.scaleX);
+				this.scene.enemyAmunition.add(babyShip);
+			},
+			callbackScope: this,
+			loop: true
+		});
 	}
+
+	onDestroy() {
+		if (this.shootTimer !== undefined) {
+			if (this.shootTimer) {
+				this.shootTimer.remove(false);
+			}
+		}
+	}
+}
+
+class BabyShip extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "babyShip");
+    this.body.setVelocity(-200,20)
+  }
 }
