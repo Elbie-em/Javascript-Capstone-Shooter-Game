9168c041536e900765bb63c3f899103ac9237d0b
Elbie-em <emoonga1993@gmail.com>
2020-09-08 15:50:50 +0200

    Start build on gameplay: Test mothership enemy in Game scene

--- src/GameScene.js
+++ src/GameScene.js
@@ -4,13 +4,26 @@ class GameScene extends Phaser.Scene {
 	}
 
 	preload() {
+		this.load.image("asteroid", "../assets/asteriod.png");
 		this.load.spritesheet("spaceShipPlayer", "../assets/playerSprite.png", {
 			frameWidth: 132,
 			frameHeight: 22
 		});
+		this.load.spritesheet("motherShip", "../assets/motherShip.png", {
+			frameWidth: 234,
+			frameHeight: 77
+		});
 	}
 
 	create() {
+
+		this.anims.create({
+			key: "motherShip",
+			frames: this.anims.generateFrameNumbers("motherShip"),
+			frameRate: 20,
+			repeat: -1
+		});
+
 		this.anims.create({
 			key: "spaceShipPlayer",
 			frames: this.anims.generateFrameNumbers("spaceShipPlayer"),
@@ -23,7 +36,25 @@ class GameScene extends Phaser.Scene {
 			this.game.config.width * 0.5,
 			this.game.config.height * 0.5,
 			"spaceShipPlayer"
-		); 
+		);
+
+		this.enemies = this.add.group();
+		this.enemyAmunition = this.add.group();
+		this.playerAmunition = this.add.group();
+
+		this.time.addEvent({
+			delay: 5000,
+			callback: function() {
+				var enemy = new MotherShip(
+					this,
+					this.game.config.width + 100,
+					Phaser.Math.Between(50, this.game.config.height)
+				);
+				this.enemies.add(enemy);
+			},
+			callbackScope: this,
+			loop: true
+		});
 
 	}
 
@@ -37,7 +68,7 @@ class GameScene extends Phaser.Scene {
 		else if (cursors.down.isDown) {
 			this.player.moveDown();
 		}
-		
+
 		if (cursors.left.isDown) {
 			this.player.moveBack();
 		}
