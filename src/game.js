105c8c3a9acfa7349bf20a7684c77aff8403499c
Elbie-em <emoonga1993@gmail.com>
2020-09-15 13:24:07 +0200

    Add game menu scene to scenes

--- src/game.js
+++ src/game.js
@@ -1,18 +1,21 @@
 const config = {
-    type: Phaser.AUTO,
-    width: 1080,
-    height: 600,
-    autoCenter: true,
-    backgroundColor: "#000000",
-    physics: {
-        default: "arcade",
-        arcade: {
-            gravity: { x: -20, y: 0 }
-        }
-    },
-    scene: [GameScene],
-    pixelArt: true,
-    roundPixels: true
+	type: Phaser.AUTO,
+	width: 1080,
+	height: 600,
+	autoCenter: true,
+	backgroundColor: "#000000",
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: { x: -20, y: 0 }
+		}
+	},
+	scene: [
+		GameMenuScene,
+		GameScene,
+	],
+	pixelArt: true,
+	roundPixels: true
 };
 
 const game = new Phaser.Game(config);
