f2d7b76569e78e72576340d02f658f29b82b8a78
Elbie-em <emoonga1993@gmail.com>
2020-09-15 13:24:32 +0200

    Add preload and create functions to game menu scene

--- src/GameMenuScene.js
+++ src/GameMenuScene.js
@@ -1,9 +1,26 @@
 class GameMenuScene extends Phaser.Scene {
     constructor() {
       super({ key: "GameMenuScene" });
-    }
+		}
+		
+		preload() {
+			this.load.image("background3", "../assets/bg.png");
+			this.load.image("btnStart", "../assets/btnStart.png");
+			this.load.image("btnStartHover", "../assets/btnStartHover.png");
+		}
   
     create() {
-      this.scene.start("GameScene");
+			this.add.image(540, 300, 'background3');
+      this.btnStart = this.add.sprite(
+				this.game.config.width * 0.5,
+				this.game.config.height * 0.5,
+				"btnStart"
+			);
+			this.btnStart.setInteractive();
+
+			this.btnStart.on("pointerover", () => {
+				this.btnStart.setTexture("btnStartHover"); 
+				//this.sfx.btnOver.play();
+			}, this);
     }
   }
