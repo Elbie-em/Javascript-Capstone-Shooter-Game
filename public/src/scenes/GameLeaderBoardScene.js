884e32a3f18c1f76fb4fb51d0857a1b3e1151202
Elbie-em <emoonga1993@gmail.com>
2020-09-23 22:23:47 +0200

    Add Game leader board scene

---
+++ public/src/scenes/GameLeaderBoardScene.js
@@ -0,0 +1,22 @@
+import Phaser from '../phaser';
+
+class GameLeaderBoardScene extends Phaser.Scene {
+  constructor() {
+		super({ key: "GameLeaderBoardScene" });
+  }
+  
+  preload() {
+    this.load.image("background3", "../assets/bg.png");
+  }
+
+  create() {
+    this.add.image(540, 300, 'background3');
+    this.title = this.add.text(this.game.config.width * 0.38, 128, "LEADER BOARD", {
+			fontFamily: 'impact',
+			fontSize: 60,
+			fontStyle: 'bold',
+			color: '#ffffff',
+			align: 'center'
+		});
+  }
+}
