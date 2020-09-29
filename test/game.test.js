7749fe8f79c3491b0b32f1fb42fc3adae762742f
Elbie-em <emoonga1993@gmail.com>
2020-09-29 19:31:37 +0200

    Add test for phaser game object

---
+++ test/game.test.js
@@ -0,0 +1,13 @@
+import startPhaserGame from "./gameMockup"
+
+let phaserGame;
+
+beforeEach(()=> {
+  phaserGame = startPhaserGame();
+});
+
+describe("testing the phaser game", () => {
+  test("check if game object exists for it to start", () => {
+    expect(typeof(phaserGame)).toBe("object");
+  })
+});
